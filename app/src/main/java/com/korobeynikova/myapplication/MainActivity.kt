529b908e8dd7b681613ab541f30d0d5732f78fc3
1101-22 <alya.korobeynikova99@gmail.com>
2024-02-10 14:20:02 +0300

    0.1

--- app/src/main/java/com/korobeynikova/myapplication/MainActivity.kt
+++ app/src/main/java/com/korobeynikova/myapplication/MainActivity.kt
@@ -2,10 +2,28 @@ package com.korobeynikova.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var no2free: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        no2free = findViewById(R.id.no2free)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.free -> {
+                    no2free.visibility = View.GONE
+                }
+                R.id.nofreeee -> {
+                    no2free.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
